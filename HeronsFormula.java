438417bb4b8acceff0ed798ba2f46e829a2aa8e0
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-02-18 21:36:20 -0500

    Add files via upload

---
+++ HeronsFormula.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HeronsFormula {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter side a:");
+        double a = keyboard.nextDouble();
+        System.out.println("Please enter side b:");
+        double b = keyboard.nextDouble();
+        System.out.println("Please enter side c:");
+        double c = keyboard.nextDouble();
+        keyboard.close();
+
+        double area = Math.sqrt(4 * a * a * b * b - Math.pow((a * a + b * b - c * c), 2)) / 4;
+        System.out.println("The area of the triangle is " + area + " units squared.");
+    }
+}
